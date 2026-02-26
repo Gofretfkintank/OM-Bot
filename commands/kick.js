6c53247d003dc4aa3c8c042aff161448cb797872
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:01:57 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,14 +3,19 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('kick')
-        .setDescription('👢 Kick a member from the server.')
-        .addUserOption(o => o.setName('target').setDescription('The user to kick').setRequired(true))
+        .setDescription('Kick a member from the server.')
+        .addUserOption(option => 
+            option.setName('target')
+                .setDescription('The member to kick')
+                .setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
     async execute(interaction) {
         await interaction.deferReply();
         const user = interaction.options.getMember('target');
+        
         if (!user.kickable) return interaction.editReply('❌ **Error:** I cannot kick this user.');
+        
         await user.kick();
-        await interaction.editReply(`👢 **${user.user.tag}** has been kicked.`);
-    }
+        await interaction.editReply(`👢 **${user.user.tag}** has been kicked from the server.`);
+    },
 };
