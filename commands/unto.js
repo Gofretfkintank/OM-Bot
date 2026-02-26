2041c6b84303ad2f2b4edd57758e40593e1137c2
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:04:14 +0300

    Update unto.js

--- commands/unto.js
+++ commands/unto.js
@@ -1,10 +1,19 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('unto').setDescription('Quickly remove a timeout.').addUserOption(o=>o.setName('user').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
-    async execute(i) {
-        await i.deferReply();
-        const m = i.options.getMember('user');
-        await m.timeout(null);
-        await i.editReply(`🔊 **${m.user.tag}** is back in the conversation! ✅`);
-    }
+    data: new SlashCommandBuilder()
+        .setName('unto')
+        .setDescription('Quickly remove a timeout from a member.')
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The user to unmute quickly')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const member = interaction.options.getMember('user');
+        
+        await member.timeout(null);
+        await interaction.editReply(`🔊 **${member.user.tag}** is back in the conversation! ✅`);
+    },
 };
