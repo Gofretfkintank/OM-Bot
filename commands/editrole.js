25319a524ae952f19366704ad346ae985700ec46
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:07:20 +0300

    Update editrole.js

--- commands/editrole.js
+++ commands/editrole.js
@@ -1,11 +1,24 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('editrole').setDescription('Edit a role name.').addRoleOption(o=>o.setName('role').setRequired(true)).addStringOption(o=>o.setName('name').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
-    async execute(i) {
-        await i.deferReply();
-        const r = i.options.getRole('role');
-        const n = i.options.getString('name');
-        await r.setName(n);
-        await i.editReply(`✏️ Role updated! New name: **${n}**`);
-    }
+    data: new SlashCommandBuilder()
+        .setName('editrole')
+        .setDescription('Rename an existing role.')
+        .addRoleOption(option => 
+            option.setName('role')
+                .setDescription('The role to rename')
+                .setRequired(true))
+        .addStringOption(option => 
+            option.setName('name')
+                .setDescription('The new name for the role')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const role = interaction.options.getRole('role');
+        const newName = interaction.options.getString('name');
+        
+        await role.setName(newName);
+        await interaction.editReply(`✏️ **Success:** Role name has been updated to **${newName}**.`);
+    },
 };
