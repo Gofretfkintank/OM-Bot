6dbb174d95fdba286aa91e52a9fe3f4fc7a4754b
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:03:54 +0300

    Update to.js

--- commands/to.js
+++ commands/to.js
@@ -1,10 +1,19 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('to').setDescription('Quick 10-minute timeout.').addUserOption(o=>o.setName('user').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
-    async execute(i) {
-        await i.deferReply();
-        const m = i.options.getMember('user');
-        await m.timeout(600000);
-        await i.editReply(`🔇 **${m.user.tag}** silenced for 10 minutes. ⚡`);
-    }
+    data: new SlashCommandBuilder()
+        .setName('to')
+        .setDescription('Quick 10-minute timeout for a member.')
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The user to silence for 10 minutes')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const member = interaction.options.getMember('user');
+        
+        await member.timeout(600000); // 10 minutes
+        await interaction.editReply(`🔇 **${member.user.tag}** has been silenced for 10 minutes. ⚡`);
+    },
 };
