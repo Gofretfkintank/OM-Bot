5129c8f70aab6dca1dde8f886e10b7ceeee752dc
Gofret <kanhasanali542@gmail.com>
2026-02-26 17:53:09 +0300

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
-    data: new SlashCommandBuilder().setName('dm').setDescription('Send a direct message through the bot.').addUserOption(o=>o.setName('user').setRequired(true)).addStringOption(o=>o.setName('msg').setRequired(true)),
+    data: new SlashCommandBuilder().setName('dm').setDescription('Send a direct message.').addUserOption(o=>o.setName('user').setDescription('User to DM').setRequired(true)).addStringOption(o=>o.setName('msg').setDescription('Message content').setRequired(true)),
     async execute(i) {
         await i.deferReply({ ephemeral: true });
         const u = i.options.getUser('user');
@@ -9,7 +9,7 @@ module.exports = {
             await u.send(`📩 **Message from ${i.guild.name}:**\n${m}`);
             await i.editReply(`✅ DM sent to **${u.tag}**.`);
         } catch {
-            await i.editReply('❌ **Failed:** User has DMs closed.');
+            await i.editReply('❌ Failed: DMs are closed.');
         }
     }
 };
