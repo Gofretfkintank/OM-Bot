a3f47a4aca2a2cfd4ce70b8767d973694eb1a648
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:02:51 +0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,10 +1,19 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('unmute').setDescription('Remove a member’s timeout.').addUserOption(o=>o.setName('user').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
-    async execute(i) {
-        await i.deferReply();
-        const m = i.options.getMember('user');
-        await m.timeout(null);
-        await i.editReply(`🔊 Timeout removed for **${m.user.tag}**. They can speak now! ✅`);
-    }
+    data: new SlashCommandBuilder()
+        .setName('unmute')
+        .setDescription('Remove timeout from a member.')
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The user to unmute')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const member = interaction.options.getMember('user');
+        
+        await member.timeout(null);
+        await interaction.editReply(`🔊 Timeout removed for **${member.user.tag}**.`);
+    },
 };
