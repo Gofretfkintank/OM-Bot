626507a7c5e0018d13495d4dbb5604266e6618ae
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:07:38 +0300

    Update give-role.js

--- commands/give-role.js
+++ commands/give-role.js
@@ -1,11 +1,24 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('give-role').setDescription('Assign a role to a user.').addUserOption(o=>o.setName('user').setRequired(true)).addRoleOption(o=>o.setName('role').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
-    async execute(i) {
-        await i.deferReply();
-        const m = i.options.getMember('user');
-        const r = i.options.getRole('role');
-        await m.roles.add(r);
-        await i.editReply(`✅ **${r.name}** assigned to **${m.user.tag}**. 🎭`);
-    }
+    data: new SlashCommandBuilder()
+        .setName('give-role')
+        .setDescription('Assign a role to a member.')
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The user to receive the role')
+                .setRequired(true))
+        .addRoleOption(option => 
+            option.setName('role')
+                .setDescription('The role to give')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const member = interaction.options.getMember('user');
+        const role = interaction.options.getRole('role');
+        
+        await member.roles.add(role);
+        await interaction.editReply(`✅ **Success:** **${role.name}** has been assigned to **${member.user.tag}**. 🎭`);
+    },
 };
