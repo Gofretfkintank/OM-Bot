003baac424f945d437b0aa042968000bcad5c9c0
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:00:07 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,12 +1,18 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('ban').setDescription('Ban a member from the server.').addUserOption(o => o.setName('target').setDescription('Member to ban').setRequired(true)).addStringOption(o => o.setName('reason').setDescription('Reason for the ban')).setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
-    async execute(i) {
-        await i.deferReply();
-        const u = i.options.getMember('target');
-        const r = i.options.getString('reason') || 'No reason provided';
-        if (!u.bannable) return i.editReply('❌ **Error:** I cannot ban this user due to role hierarchy.');
-        await u.ban({ reason: r });
-        await i.editReply(`🔨 **${u.user.tag}** has been banned. Reason: ${r}`);
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Ban a member from the server.')
+        .addUserOption(option => option.setName('target').setDescription('The member to ban').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('The reason for the ban'))
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const target = interaction.options.getMember('target');
+        const reason = interaction.options.getString('reason') || 'No reason provided';
+        if (!target.bannable) return interaction.editReply('❌ I cannot ban this user.');
+        await target.ban({ reason });
+        await interaction.editReply(`🔨 **${target.user.tag}** has been banned. Reason: ${reason}`);
     }
 };
