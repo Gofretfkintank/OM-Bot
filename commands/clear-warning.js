0ac875099c138679726d412dede8e8c0fa22cbc5
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:05:28 +0300

    Update clear-warning.js

--- commands/clear-warning.js
+++ commands/clear-warning.js
@@ -1,13 +1,26 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const fs = require('fs');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('clear-warning').setDescription('Clear all warnings for a user.').addUserOption(o=>o.setName('user').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-    async execute(i) {
-        await i.deferReply();
-        const u = i.options.getUser('user');
-        let d = JSON.parse(fs.readFileSync('./warns.json', 'utf8') || '{}');
-        delete d[u.id];
-        fs.writeFileSync('./warns.json', JSON.stringify(d, null, 2));
-        await i.editReply(`✅ All warnings cleared for **${u.tag}**. Records updated! ♻️`);
-    }
+    data: new SlashCommandBuilder()
+        .setName('clear-warning')
+        .setDescription('Completely clear all warnings for a user.')
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The user whose warnings will be deleted')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const user = interaction.options.getUser('user');
+        
+        let warns = JSON.parse(fs.readFileSync('./warns.json', 'utf8') || '{}');
+        if (warns[user.id]) {
+            delete warns[user.id];
+            fs.writeFileSync('./warns.json', JSON.stringify(warns, null, 2));
+            await interaction.editReply(`✅ All warnings for **${user.tag}** have been cleared. ♻️`);
+        } else {
+            await interaction.editReply('❌ This user has no warnings to clear.');
+        }
+    },
 };
