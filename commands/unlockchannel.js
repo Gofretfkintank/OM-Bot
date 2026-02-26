50570a3cd83207941c50680b03a618cbe94d6ca0
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:09:07 +0300

    Update unlockchannel.js

--- commands/unlockchannel.js
+++ commands/unlockchannel.js
@@ -1,9 +1,13 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('unlockchannel').setDescription('Unlock the channel for everyone.').setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
-    async execute(i) {
-        await i.deferReply();
-        await i.channel.permissionOverwrites.edit(i.guild.roles.everyone, { SendMessages: true });
-        await i.editReply('🔓 **Channel Unlocked!** Everyone can speak now. 🗣️');
-    }
+    data: new SlashCommandBuilder()
+        .setName('unlockchannel')
+        .setDescription('Unlock the channel for everyone.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    async execute(interaction) {
+        await interaction.deferReply();
+        await interaction.channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SendMessages: true });
+        await interaction.editReply('🔓 **Channel Unlocked!** Everyone can speak now. 🗣️');
+    },
 };
