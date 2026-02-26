9606adcd3cc19c140d2e6cf9dd70fba45dbe06d1
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:10:09 +0300

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -1,11 +1,24 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('nick').setDescription('Change nickname.').addUserOption(o=>o.setName('user').setDescription('Target user').setRequired(true)).addStringOption(o=>o.setName('name').setDescription('New nickname').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.ManageNicknames),
-    async execute(i) {
-        await i.deferReply();
-        const m = i.options.getMember('user');
-        const n = i.options.getString('name');
-        await m.setNickname(n);
-        await i.editReply(`📝 Nickname for **${m.user.tag}** changed to **${n}**.`);
-    }
+    data: new SlashCommandBuilder()
+        .setName('nick')
+        .setDescription('Change a member’s nickname.')
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The member to rename')
+                .setRequired(true))
+        .addStringOption(option => 
+            option.setName('name')
+                .setDescription('The new nickname')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageNicknames),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const member = interaction.options.getMember('user');
+        const newName = interaction.options.getString('name');
+        
+        await member.setNickname(newName);
+        await interaction.editReply(`📝 **Success:** Nickname for **${member.user.tag}** changed to **${newName}**.`);
+    },
 };
