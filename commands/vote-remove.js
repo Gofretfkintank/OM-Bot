a67ea10ba6997a277b471b9f0c10a47c83c01cdb
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:11:43 +0300

    Update vote-remove.js

--- commands/vote-remove.js
+++ commands/vote-remove.js
@@ -1,15 +1,24 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('vote-remove').setDescription('Delete a poll message.').addStringOption(o=>o.setName('id').setDescription('Poll message ID').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
-    async execute(i) {
-        await i.deferReply({ ephemeral: true });
-        const id = i.options.getString('id');
+    data: new SlashCommandBuilder()
+        .setName('vote-remove')
+        .setDescription('Delete a poll message using its ID.')
+        .addStringOption(option => 
+            option.setName('id')
+                .setDescription('The ID of the poll message')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
+        const messageId = interaction.options.getString('id');
+        
         try {
-            const m = await i.channel.messages.fetch(id);
-            await m.delete();
-            await i.editReply('🗑️ Poll has been removed.');
-        } catch {
-            await i.editReply('❌ **Error:** Message not found.');
+            const pollMessage = await interaction.channel.messages.fetch(messageId);
+            await pollMessage.delete();
+            await interaction.editReply('🗑️ **Success:** Poll message has been deleted.');
+        } catch (error) {
+            await interaction.editReply('❌ **Error:** Message not found or already deleted.');
         }
-    }
+    },
 };
