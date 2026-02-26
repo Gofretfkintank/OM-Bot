3913fdec262479b306454a627201c536eaad09bf
Gofret <kanhasanali542@gmail.com>
2026-02-26 16:52:50 +0300

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const fs = require('fs');
+module.exports = {
+    data: new SlashCommandBuilder().setName('warn').setDescription('Issue a warning to a member.').addUserOption(o=>o.setName('user').setRequired(true)).addStringOption(o=>o.setName('reason').setRequired(true)).setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+    async execute(i) {
+        await i.deferReply();
+        const u = i.options.getUser('user');
+        const r = i.options.getString('reason');
+        let d = JSON.parse(fs.readFileSync('./warns.json', 'utf8') || '{}');
+        if(!d[u.id]) d[u.id] = [];
+        d[u.id].push({ reason: r, moderator: i.user.tag, date: new Date().toLocaleDateString() });
+        fs.writeFileSync('./warns.json', JSON.stringify(d, null, 2));
+        await i.editReply(`⚠️ **${u.tag}** has been warned. Reason: **${r}**`);
+    }
+};
