e45c0787177933deef0b1a446b24cd1e76fce883
Gofret <kanhasanali542@gmail.com>
2026-02-26 17:52:46 +0300

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,14 +1,14 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
-    data: new SlashCommandBuilder().setName('report').setDescription('Report a user to staff.').addUserOption(o=>o.setName('user').setRequired(true)).addStringOption(o=>o.setName('reason').setRequired(true)),
+    data: new SlashCommandBuilder().setName('report').setDescription('Report a user.').addUserOption(o=>o.setName('user').setDescription('User to report').setRequired(true)).addStringOption(o=>o.setName('reason').setDescription('Reason for report').setRequired(true)),
     async execute(i) {
         await i.deferReply({ ephemeral: true });
         const u = i.options.getUser('user');
         const r = i.options.getString('reason');
         const logChannel = i.guild.channels.cache.get(process.env.REPORT_LOG_ID);
-        if(!logChannel) return i.editReply('❌ **Error:** Report log channel not found.');
-        const embed = new EmbedBuilder().setTitle('📩 New Report').addFields({name:'Reporter', value:i.user.tag}, {name:'Target', value:u.tag}, {name:'Reason', value:r}).setColor('Red').setTimestamp();
+        if(!logChannel) return i.editReply('❌ Report channel not found.');
+        const embed = new EmbedBuilder().setTitle('📩 New Report').addFields({name:'Reporter', value:i.user.tag}, {name:'Target', value:u.tag}, {name:'Reason', value:r}).setColor('Red');
         await logChannel.send({ embeds: [embed] });
-        await i.editReply('✅ Your report has been submitted to staff.');
+        await i.editReply('✅ Your report has been submitted.');
     }
 };
