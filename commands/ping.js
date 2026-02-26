363f89547d14c0a34d7fa30755e76dd0325de202
Gofret <kanhasanali542@gmail.com>
2026-02-26 18:11:03 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Check bot latency.'),
-    async execute(i) {
-        await i.deferReply();
-        await i.editReply(`🏓 **Pong!** Latency: **${i.client.ws.ping}ms**`);
-    }
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Check the bot’s current latency.'),
+    async execute(interaction) {
+        await interaction.deferReply();
+        await interaction.editReply(`🏓 **Pong!** Latency is **${interaction.client.ws.ping}ms**.`);
+    },
 };
