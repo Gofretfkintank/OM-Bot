85b925afefb10c49b5c220f1e80c8baddfaec7ce
Gofret <kanhasanali542@gmail.com>
2026-03-01 01:18:13 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -13,9 +13,14 @@ module.exports = {
         await interaction.deferReply();
         const user = interaction.options.getMember('target');
         
+        // Eğer kullanıcı sunucuda değilse botun çökmesini engellemek için kontrol
+        if (!user) return interaction.editReply('❌ **Error:** This user is not in the server.');
+        
+        // Botun yetkisi yetmiyor mu kontrolü
         if (!user.kickable) return interaction.editReply('❌ **Error:** I cannot kick this user.');
         
         await user.kick();
+        // Orijinal mesajın korunmuştur
         await interaction.editReply(`🔫 **${user.user.tag}** User session has been terminated by the OM Bot.`);
     },
 };
