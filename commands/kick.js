34f8b7bce4515d57c0aed9181956ae4190554081
Gofret <kanhasanali542@gmail.com>
2026-03-01 01:49:44 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,15 +13,16 @@ module.exports = {
         await interaction.deferReply();
         const user = interaction.options.getMember('target');
         
-        // Eğer kullanıcı sunucuda değilse botun çökmesini engellemek için kontrol
         if (!user) return interaction.editReply('❌ **Error:** This user is not in the server.');
-        
-        // Botun yetkisi yetmiyor mu kontrolü
         if (!user.kickable) return interaction.editReply('❌ **Error:** I cannot kick this user.');
         
         await user.kick();
-        // Orijinal mesajın korunmuştur
-        await interaction.editReply(`🔫 **${user.user.tag}** User session has been terminated by the OM Bot.`);
+
+        // Embed oluşturma
+        const kickEmbed = new EmbedBuilder()
+            .setColor(0x2ecc71) // Yeşil renk
+            .setDescription(`🔫 **${user.user.tag}** User session has been terminated by the OM Bot.`);
+
+        await interaction.editReply({ embeds: [kickEmbed] });
     },
 };
-// Triggering redeploy for bug fix
