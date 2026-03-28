6e67d478902fccef2e3d29aaaa18083e3a6df087
Gofret <kanhasanali542@gmail.com>
2026-03-28 15:40:09 +0300

    Update unquarantina.js

--- commands/unquarantina.js
+++ commands/unquarantina.js
@@ -8,17 +8,17 @@ const saveDB = (data) => fs.writeFileSync(dbPath, JSON.stringify(data, null, 2))
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('karantina-kaldir')
-        .setDescription('Karantinayı kaldırır.')
+        .setName('unjail')
+        .setDescription('Release a user from quarantine.')
         .addUserOption(option =>
             option.setName('target')
-                .setDescription('Kullanıcı')
+                .setDescription('User to release')
                 .setRequired(true)
         ),
 
     async execute(interaction) {
         if (!interaction.member.permissions.has(PermissionFlagsBits.Administrator)) {
-            return interaction.reply({ content: 'Yetkin yok!', ephemeral: true });
+            return interaction.reply({ content: 'You do not have permission.', ephemeral: true });
         }
 
         await interaction.deferReply({ ephemeral: true });
@@ -26,19 +26,18 @@ module.exports = {
         const targetUser = interaction.options.getUser('target');
         const targetMember = await interaction.guild.members.fetch(targetUser.id).catch(() => null);
 
-        if (!targetMember) return interaction.editReply('Kullanıcı yok.');
+        if (!targetMember) return interaction.editReply('User not found.');
 
         const db = loadDB();
         const oldRoles = db[targetMember.id];
 
         if (!oldRoles) {
-            return interaction.editReply('Bu kullanıcı karantinada değil.');
+            return interaction.editReply('This user is not jailed.');
         }
 
         try {
             await targetMember.roles.set(oldRoles);
 
-            // TÜM override'ları temizle
             for (const channel of interaction.guild.channels.cache.values()) {
                 try {
                     await channel.permissionOverwrites.delete(targetMember.id);
@@ -48,11 +47,11 @@ module.exports = {
             delete db[targetMember.id];
             saveDB(db);
 
-            return interaction.editReply(`🔓 ${targetMember.user.tag} serbest bırakıldı.`);
+            return interaction.editReply(`🔓 ${targetMember.user.tag} has been released.`);
 
         } catch (err) {
             console.error(err);
-            return interaction.editReply('Hata oluştu.');
+            return interaction.editReply('An error occurred while releasing the user.');
         }
     }
 };
