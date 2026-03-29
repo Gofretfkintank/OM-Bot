7e54ec48a1c625a336c3848f451a27635f6a7284
Gofret <kanhasanali542@gmail.com>
2026-03-29 11:09:01 +0300

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,34 +1,43 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const fs = require('fs');
+const Warn = require('../models/Warn');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('warn')
-        .setDescription('Issue a formal warning to a member.')
-        .addUserOption(option => 
-            option.setName('user')
-                .setDescription('The user to warn')
-                .setRequired(true))
-        .addStringOption(option => 
-            option.setName('reason')
-                .setDescription('The reason for the warning')
-                .setRequired(true))
+        .setDescription('Issue a warning')
+        .addUserOption(opt =>
+            opt.setName('user').setDescription('User').setRequired(true))
+        .addStringOption(opt =>
+            opt.setName('reason').setDescription('Reason').setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
     async execute(interaction) {
         await interaction.deferReply();
+
         const user = interaction.options.getUser('user');
         const reason = interaction.options.getString('reason');
-        
-        let warns = JSON.parse(fs.readFileSync('./warns.json', 'utf8') || '{}');
-        if (!warns[user.id]) warns[user.id] = [];
-        
-        warns[user.id].push({
-            reason: reason,
+
+        let data = await Warn.findOne({
+            userId: user.id,
+            guildId: interaction.guildId
+        });
+
+        if (!data) {
+            data = new Warn({
+                userId: user.id,
+                guildId: interaction.guildId,
+                warns: []
+            });
+        }
+
+        data.warns.push({
+            reason,
             moderator: interaction.user.tag,
             date: new Date().toLocaleDateString()
         });
-        
-        fs.writeFileSync('./warns.json', JSON.stringify(warns, null, 2));
-        await interaction.editReply(`⚠️ **${user.tag}** has been warned. Reason: **${reason}**`);
-    },
+
+        await data.save();
+
+        await interaction.editReply(`⚠️ ${user.tag} warned. Reason: ${reason}`);
+    }
 };
