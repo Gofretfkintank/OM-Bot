2c7267052ee689aa9adde93f012a85b49af04c46
Gofret <kanhasanali542@gmail.com>
2026-03-29 11:13:30 +0300

    Update Jail.js

--- models/Jail.js
+++ models/Jail.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const jailSchema = new mongoose.Schema({
     userId: String,
     guildId: String,
-    jailed: { type: Boolean, default: false }
+    roles: [String] // 🔥 eski roller burada tutuluyor
 });
 
 module.exports = mongoose.model('Jail', jailSchema);
