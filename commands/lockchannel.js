da175bffa7112fe25cf0f5fb24c7fc3c4ebf063e
Gofret <kanhasanali542@gmail.com>
2026-04-23 14:50:14 +0300

    Update lockchannel.js

--- commands/lockchannel.js
+++ commands/lockchannel.js
@@ -1,22 +1,32 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
+async function lockChannel(channel, guild) {
+    const nonStaff = guild.roles.cache.filter(r =>
+        !r.permissions.has(PermissionFlagsBits.ManageMessages) &&
+        !r.permissions.has(PermissionFlagsBits.Administrator) &&
+        r.name !== '@everyone'
+    );
+    for (const [, role] of nonStaff)
+        await channel.permissionOverwrites.edit(role, { SendMessages: false }).catch(() => {});
+    await channel.permissionOverwrites.edit(guild.roles.everyone, { SendMessages: false });
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('lockchannel')
         .setDescription('🔒 Lock the channel from all non-staff roles.')
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+
     async execute(interaction) {
         await interaction.deferReply();
-        const nonStaff = interaction.guild.roles.cache.filter(r => 
-            !r.permissions.has(PermissionFlagsBits.ManageMessages) && 
-            !r.permissions.has(PermissionFlagsBits.Administrator) &&
-            r.name !== '@everyone'
-        );
-
-        for (const [id, role] of nonStaff) {
-            await interaction.channel.permissionOverwrites.edit(role, { SendMessages: false }).catch(console.error);
-        }
-        await interaction.channel.permissionOverwrites.edit(interaction.guild.roles.everyone, { SendMessages: false });
+        await lockChannel(interaction.channel, interaction.guild);
         await interaction.editReply('🛡️ **Channel Locked!** All non-staff roles have been restricted. 🔒');
+    },
+
+    async prefix(message) {
+        if (!message.member.permissions.has(PermissionFlagsBits.ManageChannels))
+            return message.reply('❌ You need **Manage Channels** permission.');
+        await lockChannel(message.channel, message.guild);
+        return message.reply('🔒 **Channel Locked!** All non-staff roles have been restricted.');
     }
 };
